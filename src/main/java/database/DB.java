95800167d60678381cd3d0eb76d744f0f704a4bd
Muhammadqodir <realkings1007@gmail.com>
2022-05-25 01:31:20 +0500

    Created

---
+++ src/main/java/database/DB.java
@@ -0,0 +1,21 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+
+  public static Connection getConnection(){
+Connection connection=null;
+      try {
+          Class.forName("org.postgresql.Driver");
+         connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Epam", "postgres", "1007");
+
+      } catch (SQLException | ClassNotFoundException e) {
+          e.printStackTrace();
+      }
+
+return connection;
+  }
+}
