b4a3af4e1c3b5193156c5f7893b9d0c9aa98a7dc
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-05-27 18:14:51 +0500

    Update DB.java
    Added SQLException and changing Date type methods

--- src/main/java/database/DB.java
+++ src/main/java/database/DB.java
@@ -1,8 +1,10 @@
 package database;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class DB {
 
@@ -10,12 +12,33 @@ public class DB {
         Connection connection = null;
         try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Epam", "postgres", "root");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/epam", "postgres", "root");
 
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
         return connection;
+    }
+
+    public static void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+
+
+    public static java.sql.Date getUtilDate(java.util.Date utilDate) {
+        return new java.sql.Date(utilDate.getTime());
     }
 }
