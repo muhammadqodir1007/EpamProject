0b75812ede9947c24cecf0bbb41f591164063e9d
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-05-27 19:35:24 +0500

    Added dao services

--- src/main/java/service/product/ProductService.java
+++ src/main/java/service/product/ProductService.java
@@ -1,9 +1,13 @@
 package service.product;
 
-import entity.PeopleBean;
+import entity.Product;
 
 import java.util.List;
 
 public interface ProductService {
-
+    List<Product> getAllProduct();
+    Product getOne(Long id);
+    boolean save(Product product);
+    boolean delete(Long id);
+    String updateProduct(Long id,Product product);
 }
