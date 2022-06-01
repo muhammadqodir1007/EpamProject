4a153fa413a5d7aa5d7d15b874e5fb8e853d7882
Muhammadqodir <realkings1007@gmail.com>
2022-06-01 23:29:00 +0500

    admin page created\

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,10 +1,40 @@
-import java.sql.SQLException;
+import javax.imageio.stream.ImageOutputStream;
+import java.io.*;
+import java.sql.*;
 
 public class MainTest {
 
+    private static final long serialVersionUID = 1L;
+    final static String URL = "jdbc:postgresql://localhost/Epam";
+    final static String USER = "postgres";
+    final static String PASSWORD = "1007";
+    final static String DRIVER = "org.postgresql.Driver";
+   static Connection conn = null;
+
+
     public static void main(String[] args) throws SQLException {
-//        boolean b = PeopleDb.existByEmail("something@gmail.com");
-//        System.out.println(b);
+        byte[] bytes=null;
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("select photo from public.\"Product\" where id=10");
+
+            while (resultSet.next()){
+                bytes = resultSet.getBytes(1);
+
+            }
+            File file=new File("D:\\img1.jpg");
+           OutputStream outputStream=new FileOutputStream(file);
+           outputStream.write(bytes);
+           outputStream.close();
+
+
+
+        } catch (ClassNotFoundException | SQLException | IOException e) {
+            e.printStackTrace();
+
+        }
 
     }
 }
--- src/main/java/service/product/ProductService.java
+++
@@ -1,9 +0,0 @@
-package service.product;
-
-import entity.PeopleBean;
-
-import java.util.List;
-
-public interface ProductService {
-
-}
