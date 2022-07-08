b8d88435bb29d0b46b372255b568efa8b6329e89
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-07-08 16:27:41 +0500

    Update data.sql
    Added EmailConfir table

---
+++ sql/data.sql
@@ -0,0 +1,43 @@
+CREATE TABLE public.users
+(
+    id bigserial NOT NULL PRIMARY KEY ,
+    username character varying(255) NOT NULL,
+    "fullName" character varying(255) NOT NULL,
+    password character varying(255) NOT NULL,
+    "phoneNumber" character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
+CREATE TABLE public.product
+(
+    id bigserial NOT NULL PRIMARY KEY ,
+    titles character varying(255) NOT NULL,
+    textData text NOT NULL,
+    description character varying(255) NOT NULL,
+    sourcelinkTo character varying(255) NOT NULL,
+    photofile bytea[] NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    category_id integer
+)
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TABLE public."EmailConfir"
+(
+    id bigserial NOT NULL,
+    email character varying(225) NOT NULL,
+    PRIMARY KEY (id)
+);
