6bb472901bc153ed93d6dfbb1150cc7ad39d6d8c
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-07-10 23:26:12 +0500

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -21,6 +21,21 @@ CREATE TABLE public.product
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     category_id integer
 )
+CREATE TABLE public.product
+(
+    id bigserial NOT NULL PRIMARY KEY ,
+    nameOf character varying(255) NOT NULL,
+    address text NOT NULL,
+    description character varying(255) NOT NULL,
+    phoneNumber character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
+    password character varying(255) NOT NULL,
+    created_At TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_At TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
+
+
+
 
 CREATE OR REPLACE FUNCTION trigger_set_timestamp()
 RETURNS TRIGGER AS $$
