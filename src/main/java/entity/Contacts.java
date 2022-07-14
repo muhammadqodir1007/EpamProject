d5777a852a467ac814a08a0366eac7b9a0ba979e
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-07-14 17:01:05 +0500

    Create Contacts.java

---
+++ src/main/java/entity/Contacts.java
@@ -0,0 +1,69 @@
+package entity;
+
+import java.util.Date;
+
+public class Contacts {
+    private long id;
+    private String username;
+    private String email;
+    private String textResponse;
+    private Date created_at;
+    private Date updated_at;
+
+    public Contacts() {
+    }
+
+    public Contacts(String username, String email, String textResponse) {
+        this.username = username;
+        this.email = email;
+        this.textResponse = textResponse;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTextResponse() {
+        return textResponse;
+    }
+
+    public void setTextResponse(String textResponse) {
+        this.textResponse = textResponse;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+}
