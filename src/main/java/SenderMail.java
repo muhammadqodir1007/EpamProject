db0cc7b62119d402c4e9e237518dd73435ee0abc
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-07-18 11:37:36 +0500

    Update SenderMail.java

--- src/main/java/SenderMail.java
+++ src/main/java/SenderMail.java
@@ -15,24 +15,8 @@ import java.util.Date;
 public class SenderMail {
 
     public static void main(String[] args) throws ParseException {
-        ProductServiceImpl productService=new ProductServiceImpl();
-        DateFormat dateFormat= new SimpleDateFormat("yyyy-mm-dd hh:mm:ss.a");
-        String time=dateFormat.format(productService.getProductByID(15).getCreated_at());
-//        SimpleDateFormat dateFormat1=new SimpleDateFormat("yyyy-mm-dd hh:mm:ss.a");
-        Date date=dateFormat.parse(time);
-        System.out.println(date);
-////        Date date=productService.getProductByID(15).getCreated_at();
-////        Date date1=dateFormat.parse(time);
-////        System.out.println(date1);
-        System.out.println(time);
-        System.out.println(productService.getProductByID(15).getCreated_at());
-//        Date date = Calendar.getInstance().getTime();
-//        String strDate = dateFormat.format(date);
-//        System.out.println("Converted String: " + strDate);
-
-
-//        System.out.println(time);
-//        DateFormat.getDateInstance().format(productService.getProductByID(15).getCreated_at());
-//        System.out.println(productService.getProductByID(15).getCreated_at());
+        UserService userService = new UserService();
+        Users users = new Users("boxobek", "Boxodir jorayev", "1", "994612309", "boxo@mail.ru");
+        System.out.println(userService.saveUser(users));
     }
 }
