1edaa1da51b438dfc7d39e172722602cefde3aea
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-07-23 15:19:47 +0500

    Update PublisherServices.java

--- src/main/java/service/publisher/PublisherServices.java
+++ src/main/java/service/publisher/PublisherServices.java
@@ -34,28 +34,73 @@ public class PublisherServices {
         return preparedStatement;
     }
 
-    public List<Publisher> getAllPublisher()
-    {
-        List<Publisher> getAllPublishers=new ArrayList<>();
+    public List<Publisher> getViewPublishers(int offset, int noOfRecords) {
+        String s = "SELECT id, \"nameOfCompany\", address, description," +
+                " phonenumber, email, password, created_at, " +
+                "updated_at, username, \"isActive\", \"isBlocked\"\n" +
+                "\tFROM public.publisher order by id limit " + noOfRecords + " offset " + offset + "";
+        List<Publisher> publisherList = new ArrayList<Publisher>();
+        Publisher publisher = null;
+        try {
+            Connection connection = DB.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(s);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                publisher = new Publisher();
+                publisher.setId(resultSet.getLong("id"));
+                publisher.setUsername(resultSet.getString("username"));
+                publisher.setPhoneNumber(resultSet.getString("phoneNumber"));
+                publisher.setEmail(resultSet.getString("email"));
+                publisher.setPassword(resultSet.getString("password"));
+                publisher.setNameOfCompany(resultSet.getString("nameOfCompany"));
+                publisher.setDescription(resultSet.getString("description"));
+                publisher.setActive(resultSet.getBoolean("isActive"));
+                publisher.setCreated_At(resultSet.getTimestamp("created_At"));
+                publisher.setAddress(resultSet.getString("address"));
+                publisher.setUpdated_At(resultSet.getTimestamp("updated_At"));
+                publisherList.add(publisher);
+            }
+            resultSet.close();
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return publisherList;
+    }
+
+    public long getnoOfRecords() {
+        long noOfRecords = 0;
+        try {
+            Connection connection = DB.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT count(*) FROM public.publisher ");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next())
+                noOfRecords=resultSet.getLong("count");
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return noOfRecords;
+    }
+
+    public List<Publisher> getAllPublisher() {
+        List<Publisher> getAllPublishers = new ArrayList<>();
         try {
             ResultSet rs = getresultSet(GET_ALL_PUBLISHERS);
-            while (rs.next())
-            {
-                long id= rs.getLong("id");
-                String username=rs.getString("username");
-                String nameOfCompany=rs.getString("nameOfCompany");
-                String address=rs.getString("address");
-                String phoneNumber=rs.getString("phoneNumber");
-                String email=rs.getString("email");
-                String password=rs.getString("password");
-                String description=rs.getString("description");
-                boolean isActive=(rs.getBoolean("isActive")!=true)?false:true;
-                getAllPublishers.add(new Publisher(id,username,nameOfCompany,
-                        address,phoneNumber,email,password,description,isActive));
+            while (rs.next()) {
+                long id = rs.getLong("id");
+                String username = rs.getString("username");
+                String nameOfCompany = rs.getString("nameOfCompany");
+                String address = rs.getString("address");
+                String phoneNumber = rs.getString("phoneNumber");
+                String email = rs.getString("email");
+                String password = rs.getString("password");
+                String description = rs.getString("description");
+                boolean isActive = (rs.getBoolean("isActive") != true) ? false : true;
+                getAllPublishers.add(new Publisher(id, username, nameOfCompany,
+                        address, phoneNumber, email, password, description, isActive));
             }
-
-        }
-        catch (SQLException exception) {
+            rs.close();
+        } catch (SQLException exception) {
             DB.printSQLException(exception);
         }
         return getAllPublishers;
@@ -77,6 +122,7 @@ public class PublisherServices {
                 publishers.setEmail(resultSet.getString("email"));
                 publishers.setPassword(resultSet.getString("password"));
             }
+            resultSet.close();
         } catch (SQLException exception) {
             DB.printSQLException(exception);
         }
@@ -99,6 +145,7 @@ public class PublisherServices {
                 publishersPublisher.setEmail(resultSet.getString("email"));
                 publishersPublisher.setPassword(resultSet.getString("password"));
             }
+            resultSet.close();
         } catch (SQLException exception) {
             DB.printSQLException(exception);
         }
