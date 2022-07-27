50e92062c38637878a2c6462538c3ffea0776dc5
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-07-27 12:54:04 +0500

    Create UserChat.java

---
+++ src/main/java/entity/UserChat.java
@@ -0,0 +1,12 @@
+package entity;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class UserChat {
+    private UUID id;
+    private String msgText;
+    private Date postDate;
+    private Integer publisher_id;
+    private Integer user_id;
+}
